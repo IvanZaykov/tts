5c9c6e757c164473809830d0bfdfb1eff16bd13c
ivan <ivan@goodiebox.dk>
2021-08-21 09:19:20 +0200

    refactoring and filtering unused languages

--- js/main.js
+++ js/main.js
@@ -1,86 +1,95 @@
 const synth = window.speechSynthesis;
 
-const textForm = document.querySelector('form');
-const textInput = document.querySelector('#text-input');
-const speakIt = document.querySelector('#speak-it');
-const voiceSelect = document.querySelector('#voice-select');
-const rate = document.querySelector('#rate');
-const rateValue = document.querySelector('#rate-value');
-const volume = document.querySelector('#volume');
-const volumeValue = document.querySelector('#volume-value');
-const pitch = document.querySelector('#pitch');
-const pitchValue = document.querySelector('#pitch-value');
-const body = document.querySelector('body');
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const speakIt = document.querySelector("#speak-it");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const volume = document.querySelector("#volume");
+const volumeValue = document.querySelector("#volume-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+const body = document.querySelector("body");
 
-let voices =[];
+let voices = [];
 
 const getVoices = () => {
   voices = synth.getVoices();
 
   // Loop through voices and create an option for each one
-  voices.forEach(voice => {
+  voices.forEach((voice) => {
     // Create option element
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     // Fill option with voice and language
-    option.textContent = voice.name + '(' + voice.lang + ')';
+    option.textContent = voice.name;
+    console.log(voice.name);
+    console.log(voice.lang);
 
     // Set needed option attributes
-    option.setAttribute('data-lang', voice.lang);
-    option.setAttribute('data-name', voice.name);
-    voiceSelect.appendChild(option);
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    if (
+      voice.name.includes("Natural") &&
+      (voice.lang === "en-GB" || voice.lang === "en-US")
+    ) {
+      voiceSelect.appendChild(option);
+    }
   });
-  voiceSelect.selectedIndex = 3;
 };
 
 getVoices();
 if (synth.onvoiceschanged !== undefined) {
-    synth.onvoiceschanged = getVoices;
+  synth.onvoiceschanged = getVoices;
 }
 
 const speak = () => {
-    if (synth.speaking) {
-        console.error('already speaking...');
-        return;
-    }
-    if (textInput.value !== '') {
-        const speakText = new SpeechSynthesisUtterance(textInput.value);
+  if (synth.speaking) {
+    console.error("already speaking...");
+    return;
+  }
+  if (textInput.value !== "") {
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
 
-        speakText.onend = e => {
-            console.log('done speaking');
-        }
-        speakText.onerror = e => {
-            console.error('something went wrong');
-        }
-        const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    speakText.onend = (e) => {
+      console.log("done speaking");
+    };
+    speakText.onerror = (e) => {
+      console.error("something went wrong");
+    };
+    const selectedVoice =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
 
-        voices.forEach(voice => {
-            if (voice.name === selectedVoice) {
-                speakText.voice = voice;
-            }
-        });
+    voices.forEach((voice) => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
 
-        speakText.rate =rate.value;
-        speakText.pitch= pitch.value;
-        speakText.volume= volume.value;
-        synth.speak(speakText)
-    }
-}
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+    speakText.volume = volume.value;
+    synth.speak(speakText);
+  }
+};
 
-textForm.addEventListener('submit', e =>{
-    e.preventDefault()
-    speak()
-    textInput.getBoundingClientRect()
-})
-textInput.addEventListener("keydown", e => { 
-    if (e.ctrlKey && e.keyCode == 13) {
-        e.preventDefault()
-        speak()
-        textInput.getBoundingClientRect()
-    }
+textForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  speak();
+  textInput.getBoundingClientRect();
+});
+textInput.addEventListener("keydown", (e) => {
+  if (e.ctrlKey && e.keyCode == 13) {
+    e.preventDefault();
+    speak();
+    textInput.getBoundingClientRect();
+  }
 });
 
-rate.addEventListener('change', e => rateValue.textContent = rate.value)
-pitch.addEventListener('change', e => rateValue.textContent = pitch.value)
-volume.addEventListener('change', e => volumeValue.textContent = volume.value)
-voiceSelect.addEventListener('change', e => speak())
-
+rate.addEventListener("change", (e) => (rateValue.textContent = rate.value));
+pitch.addEventListener("change", (e) => (rateValue.textContent = pitch.value));
+volume.addEventListener(
+  "change",
+  (e) => (volumeValue.textContent = volume.value)
+);
+voiceSelect.addEventListener("change", (e) => speak());
